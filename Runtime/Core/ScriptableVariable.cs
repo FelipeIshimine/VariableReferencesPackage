7a4b4e620530c2b9d6fc808584e5c049dcd1e938
Ishimine <felipecalafate@hotmail.com>
2024-10-04 20:12:37 -0300

    asd

--- Runtime/Core/ScriptableVariable.cs
+++ Runtime/Core/ScriptableVariable.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Cartographer.Utilities.Attributes;
+using UnityEngine;
 
 namespace VariableReferences
 {
@@ -6,6 +7,35 @@ namespace VariableReferences
 	{
 		[field: SerializeField] private T defaultValue;
 		[field: SerializeField] private T liveValue;
+		
+		public T Value
+		{
+			get => Application.isPlaying ? liveValue : defaultValue;
+			set
+			{
+				if (Application.isPlaying)
+					liveValue = value;
+				else
+					defaultValue = value;
+			}
+		}
+
+		private void OnEnable()
+		{
+			liveValue = defaultValue;
+		}
+
+		private void OnDisable()
+		{
+			liveValue = default;
+		}
+	}
+	
+	
+	public class ScriptableVariableReference<T> : ScriptableObject, IVariableReference<T>
+	{
+		[field: SerializeReference, TypeDropdown] private T defaultValue;
+		[field: SerializeReference] private T liveValue;
 		
 		public T Value
 		{
